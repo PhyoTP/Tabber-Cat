ae6033f2a955dfad09b372e5a51411896978374d
Phyo Thet Pai <phyo.thet.pai@sji.edu.sg>
2023-09-06 10:21:03 +0800

    Initial Commit

---
+++ Tabber Cat/Tabber_CatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tabber_CatApp.swift
+//  Tabber Cat
+//
+//  Created by T Krobot on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Tabber_CatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
